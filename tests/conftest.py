f4b171c2c305459dccfe066937e2c3d31399b505
Marcus Pereira <mpereirassa@gmail.com>
2020-04-01 02:11:59 -0300

    first commiit

---
+++ tests/conftest.py
@@ -0,0 +1,30 @@
+import pytest
+
+from application import create_app
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+@pytest.fixture
+def client():
+    app = create_app()
+    app.config["TESTING"] = True
+    ctx = app.app_context()
+    ctx.push()
+
+    yield app.test_client()
+    
+    ctx.pop()
+
+
+@pytest.yield_fixture
+def web_client():
+    options = Options()
+    options.headless = True
+    chrome = webdriver.Chrome(options=options)
+    chrome.implicitly_wait(3)
+
+    yield chrome
+
+    chrome.close()
+    chrome.quit()
---
+++ tests/test_flask_vuejs.py
@@ -0,0 +1,35 @@
+from flask_vuejs import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
+
+
+def test_import_vue():
+    try:
+        from flask_vue import Vue
+    except ImportError:
+        pass
+
+
+def test_index_route_return_status_code_200(client):
+    res = client.get("/")
+    assert res.status_code == 200
+
+
+def test_index_route_return_true_when_app_id_is_found(client):
+    res = client.get("/")
+    assert 'id="app"' in res.get_data(as_text=True)
+
+
+def test_index_route_return_true_if_component_was_imported(client):
+    res = client.get("/")
+    assert '<script src="/static/components/test.js"></script>' in res.get_data(
+        as_text=True
+    )
+
+def test_index_route_return_true_if_form_was_rendered(web_client):
+    web_client.get('http://localhost:5000')
+    web_client.find_element_by_name("task")
+    web_client.find_element_by_id("form-vue")
+    web_client.find_element_by_tag_name("form")
