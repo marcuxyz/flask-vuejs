c5025a95082e7ccabb1028161e3117a5d90939d9
Marcus Pereira <mpereirassa@gmail.com>
2020-04-01 03:13:42 -0300

    update tests

--- tests/test_flask_vuejs.py
+++ tests/test_flask_vuejs.py
@@ -19,12 +19,16 @@ def test_index_route_return_true_when_app_id_is_found(client):
 
 def test_index_route_return_true_if_component_was_imported(client):
     res = client.get("/")
-    assert '<script src="/static/components/test.js"></script>' in res.get_data(
+    assert "<script src='/static/components/form.js'></script>" in res.get_data(
         as_text=True
     )
 
+def test_index_route_get_component_title(web_client):
+    web_client.get('http://localhost:5000/')
+    assert "Task Form" in web_client.page_source
+
 def test_index_route_return_true_if_form_was_rendered(web_client):
-    web_client.get('http://localhost:5000')
+    web_client.get('http://localhost:5000/')
     web_client.find_element_by_name("task")
     web_client.find_element_by_id("form-vue")
     web_client.find_element_by_tag_name("form")
