134e4facb274fb0729acdfda393b6280c21a78e3
Marcus Pereira <mpereirassa@gmail.com>
2020-04-01 02:58:20 -0300

    set delimiters for vue

---
+++ flask_vuejs/static/core/config.js
@@ -0,0 +1,9 @@
+/**
+ * Organization: Pacotei
+ * Github: https://github.com/orgs/pacotei
+ * Author: Marcus Pereira
+ * E-mail: oi@pacotei.app
+ */
+
+//  Set Confi for Vue
+Vue.options.delimiters = ["${", "}"];
