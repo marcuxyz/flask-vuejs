039df598a7381449b6bbada3f43497c0f71f30c4
Marcus Pereira <mpereirassa@gmail.com>
2020-04-01 03:58:44 -0300

    fix format

--- tests/test_flask_vuejs.py
+++ tests/test_flask_vuejs.py
@@ -1,5 +1,3 @@
-
-
 def test_import_vue():
     try:
         from flask_vue import Vue
@@ -23,12 +21,14 @@ def test_index_route_return_true_if_component_was_imported(client):
         as_text=True
     )
 
+
 def test_index_route_get_component_title(web_client):
-    web_client.get('http://localhost:5000/')
+    web_client.get("http://localhost:5000/")
     assert "Task Form" in web_client.page_source
 
+
 def test_index_route_return_true_if_form_was_rendered(web_client):
-    web_client.get('http://localhost:5000/')
+    web_client.get("http://localhost:5000/")
     web_client.find_element_by_name("task")
     web_client.find_element_by_id("form-vue")
     web_client.find_element_by_tag_name("form")
