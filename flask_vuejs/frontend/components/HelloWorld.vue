095a4c9b4c2b0780edaaea398c5f1ac7d9d51321
Marcus Pereira <mpereirassa@gmail.com>
2020-04-03 18:06:01 -0300

    create mockups

---
+++ flask_vuejs/frontend/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    Hello {name}
+  </div>
+</template>
+
+<script>
+import Axios from "axios";
+
+export default {
+  name: "HelloWorld",
+  props: [
+    "name"
+  ]
+};
+</script>
