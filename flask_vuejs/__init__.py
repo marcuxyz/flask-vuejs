e69d80db2c077d9dfae036f4c25743e924606583
Marcus Pereira <mpereirassa@gmail.com>
2020-04-03 18:01:22 -0300

    remove environs settings

--- flask_vuejs/__init__.py
+++ flask_vuejs/__init__.py
@@ -1,7 +1,4 @@
 from flask import Blueprint, Flask
-from flask_assets import Bundle, Environment
-
-from . import environs
 
 
 class Vue:
@@ -13,8 +10,6 @@ class Vue:
         app.config.setdefault("FLASK_VUE_COMPONENT_DIRECTORY", "components")
 
         self.register_vue_static_url(app)
-        self.register_global_object(app)
-        self.build_js(app)
 
     def register_vue_static_url(self, app):
         app.register_blueprint(
@@ -26,16 +21,3 @@ class Vue:
                 template_folder="templates",
             )
         )
-
-    def register_global_object(self, app):
-        app.jinja_env.globals["use_component"] = environs.load_component_file
-
-    def build_js(self, app):
-        environ = Environment(app)
-        js = Bundle(
-            "vue/core/vue.js",
-            "vue/core/axios.js",
-            "vue/core/config.js",
-            output="vue/main.js",
-        )
-        environ.register("build_js", js)
