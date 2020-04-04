a7b6d4c1cb83c91a88aa06b67651a865537b1149
Marcus Pereira <mpereirassa@gmail.com>
2020-04-04 13:48:05 -0300

    fix #7 - add css support within a component

--- flask_vuejs/init/webpack.config.js
+++ flask_vuejs/init/webpack.config.js
@@ -32,6 +32,10 @@ module.exports = {
         test: /\.vue$/,
         loader: "vue-loader",
       },
+      {
+        test: /\.css$/,
+        use: ["vue-style-loader", "css-loader"],
+      },
     ],
   },
 
